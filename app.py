9938c1619fb0cbf7533cdd8195105367fa2bd43a
Paulo Craice <paulocraice@gmail.com>
2025-05-11 00:21:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://example.com")
+    title = driver.title
+    driver.quit()
+    
+    return f"<h1>Page title: {title}</h1>"
